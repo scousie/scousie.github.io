f96d9eef627846dec1a89afe9b761157a9f9b64d
scousie <49002531+scousie@users.noreply.github.com>
2023-02-16 20:57:50 +0000

    modifications to scrape_morgen

--- scrape_morgen.js
+++ scrape_morgen.js
@@ -1,34 +1,21 @@
 const cheerio = require('cheerio');
-const request = require('request');
+const fs = require('fs');
 
-/* @scousie ➜ /workspaces/scousie.github.io (main) $ npm install request
-npm WARN deprecated har-validator@5.1.5: this library is no longer supported
-npm WARN deprecated uuid@3.4.0: Please upgrade  to version 7 or higher.  Older versions may use Math.random() in certain circumstances, which is known to be problematic.  See https://v8.dev/blog/math-random for details.
-npm WARN deprecated request@2.88.2: request has been deprecated, see https://github.com/request/request/issues/3142
-*/
+async function scrapeMorgen(linkske) {
+  const response = await fetch(linkske);
+  const body = await response.text();
+  const $ = cheerio.load(body);
+  const paragraphs = $('p.artstyle__paragraph');
 
-// URL of the website you want to scrape
-const url = 'https://www.demorgen.be/snelnieuws/frustratie-over-minder-lesuren-aardrijkskunde-en-geschiedenis-dit-is-een-catastrofe~b6d7699e/';
+  const output = [];
 
-// Make a request to the website
-request(url, (error, response, html) => {
-  // Check for errors and status code
-  if (!error && response.statusCode == 200) {
-    // Load the HTML into Cheerio
-    const $ = cheerio.load(html);
+  // Add each paragraph to the output array
+  paragraphs.each((i, el) => {
+      output.push($(el).text());
+  });
 
-    // Select all the paragraphs with the 'artstyle__paragraph' class
-    const paragraphs = $('p.artstyle__paragraph');
+  // Write the output array to a text file
+  fs.writeFileSync('output.txt', output.join('\n'));
+}
 
-    // Loop through the paragraphs and log the text content of each one
-    paragraphs.each((i, el) => {
-      console.log($(el).text());
-    });
-  }
-});
-
-/* @scousie ➜ /workspaces/scousie.github.io (main) $ npm install request
-npm WARN deprecated har-validator@5.1.5: this library is no longer supported
-npm WARN deprecated uuid@3.4.0: Please upgrade  to version 7 or higher.  Older versions may use Math.random() in certain circumstances, which is known to be problematic.  See https://v8.dev/blog/math-random for details.
-npm WARN deprecated request@2.88.2: request has been deprecated, see https://github.com/request/request/issues/3142
-*/
+scrapeMorgen('https://www.demorgen.be/snelnieuws/frustratie-over-minder-lesuren-aardrijkskunde-en-geschiedenis-dit-is-een-catastrofe~b6d7699e');
